7ff65feb56c0bbcbf815b753055edf4a5df73a4b
¨Mario Castro¨ <mnc634@utexas.edu>
2020-12-09 01:00:50 -0600

    added Manager function and prompt questions

--- Develop/app.js
+++ Develop/app.js
@@ -14,89 +14,35 @@ const render = require("./lib/htmlRenderer");
 // and to create objects for each team member (using the correct classes as blueprints!)
 const inquirer = require("inquirer");
 
-var selectTeamMember = [
+console.log ("Please build your team");
+
+var setManager = [
+
     {
-      type: 'confirm',
-      name: 'toBeDelivered',
-      message: 'Is this for delivery?',
-      default: false,
+        type: 'input',
+        name: 'managerName',
+        message: "What is your manager's name?",
     },
     {
-      type: 'input',
-      name: 'phone',
-      message: "What's your phone number?",
-      validate: function (value) {
-        var pass = value.match(
-          /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
-        );
-        if (pass) {
-          return true;
-        }
-  
-        return 'Please enter a valid phone number';
-      },
+        type: 'input',
+        name: 'managerID',
+        message: "What is your manager's ID number?",
     },
     {
-      type: 'list',
-      name: 'size',
-      message: 'What size do you need?',
-      choices: ['Large', 'Medium', 'Small'],
-      filter: function (val) {
-        return val.toLowerCase();
-      },
+        type: 'input',
+        name: 'managerEmail',
+        message: "What is your manager's email address?",
     },
     {
-      type: 'input',
-      name: 'quantity',
-      message: 'How many do you need?',
-      validate: function (value) {
-        var valid = !isNaN(parseFloat(value));
-        return valid || 'Please enter a number';
-      },
-      filter: Number,
-    },
-    {
-      type: 'expand',
-      name: 'toppings',
-      message: 'What about the toppings?',
-      choices: [
-        {
-          key: 'p',
-          name: 'Pepperoni and cheese',
-          value: 'PepperoniCheese',
-        },
-        {
-          key: 'a',
-          name: 'All dressed',
-          value: 'alldressed',
-        },
-        {
-          key: 'w',
-          name: 'Hawaiian',
-          value: 'hawaiian',
-        },
-      ],
-    },
-    {
-      type: 'rawlist',
-      name: 'beverage',
-      message: 'You also get a free 2L beverage',
-      choices: ['Pepsi', '7up', 'Coke'],
-    },
-    {
-      type: 'input',
-      name: 'comments',
-      message: 'Any comments on your purchase experience?',
-      default: 'Nope, all good!',
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: "What is your manager's office number?",
     },
     {
       type: 'list',
-      name: 'prize',
-      message: 'For leaving a comment, you get a freebie',
-      choices: ['cake', 'fries'],
-      when: function (answers) {
-        return answers.comments !== 'Nope, all good!';
-      },
+      name: 'typeMember',
+      message: "Which type of team member would you like to add?",
+      choices: ['Engineer', 'Intern', "I don't want any other member"],
     },
   ];
   
